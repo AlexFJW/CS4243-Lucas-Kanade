bbf2d970125db383b2921b8416b8bfeda9db855d
Nicholette <nekonekonik@gmail.com>
2017-11-12 23:01:19 +0800

    Churned out videos with different thresholds

--- shootingStar.m
+++ shootingStar.m
@@ -1,5 +1,6 @@
-video = VideoReader('videos/Bar swing FAIL.mp4');
-video.CurrentTime = 4.5; %lesser presence of ghost on the swing bars
+% video = VideoReader('videos/trimmed/swing_1.mp4');
+video = VideoReader('videos/trimmed/jumping_new_2.mp4');
+% video = VideoReader('videos/trimmed/playground.mp4');
 currAxes = axes;
 frameMatrix = zeros(video.height, video.width, 3);
 firstFrame = zeros(video.height, video.width, 3);
@@ -13,11 +14,9 @@ if hasFrame(video)
     firstFrame = frameMatrix;
     while hasFrame(video)
         currentFrame = readFrame(video);
-        currentMatrix = (cast(frameMatrix, 'double') * weightage) ...
-                        + (cast(currentFrame, 'double'));
+        currentMatrix = (double(frameMatrix) * weightage) + double(currentFrame);
         averageMatrix = currentMatrix / counter;
         
-
         frameMatrix = averageMatrix;
         counter = counter + 1;
         weightage = weightage + 1;
@@ -27,32 +26,41 @@ else
 end
 
 counter = 2;
-video = VideoReader('videos/Bar swing FAIL.mp4');
+threshold = 30;
+% video = VideoReader('videos/trimmed/swing_1.mp4');
+video = VideoReader('videos/trimmed/jumping_new_2.mp4');
+% video = VideoReader('videos/trimmed/playground.mp4');
+
+% outputVideo = VideoWriter('swing/swing_out_new_2_65.mp4', 'MPEG-4');
+outputVideo = VideoWriter('jumping/jumping_out_new_2_30.mp4', 'MPEG-4');
+% outputVideo = VideoWriter('playground/playground_40.mp4', 'MPEG-4');
+outputVideo.FrameRate = video.FrameRate;
+open(outputVideo)
+
 if hasFrame(video)
     while hasFrame(video)
-        currentFrame = cast(readFrame(video), 'double');
-        movingObjects = abs(cast(currentFrame, 'double') - averageMatrix);
+        currentFrame = double(readFrame(video));
+        diff = abs(double(currentFrame) - averageMatrix);
         
-        if(mod(counter, 20) == 0)
-            figure;
-            imshow(cast(255 - abs(currentFrame-averageMatrix), 'uint8'))
-
+        [x,y,z] = size(diff);
+        
+        for i = 1:x
+            for j = 1:y
+                if (sum(diff(i,j,:)) <= threshold)
+                    diff(i,j,:) = 0;
+                else
+                    diff(i,j,:) = 1;
+                end
+            end
         end
+
+        movingObjects = currentFrame .* double(diff);
+        
+        writeVideo(outputVideo, uint8(movingObjects));
         counter = counter + 1;
     end
 else
     disp('video is empty');
 end
 
-
-firstMovingImage = abs(cast(firstFrame, 'double') - frameMatrix);
-
-figH = figure;
-imshow(cast(firstFrame, 'uint8'));
-figI = figure;
-imshow(cast(firstMovingImage, 'uint8'));
-
-figJ = figure;
-imshow(cast(frameMatrix, 'uint8'));
-figK = figure;
-imshow(cast(movingObjects, 'uint8'));
+close(outputVideo);
