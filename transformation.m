ca32933351ca94d61a1732e05bd8e2e819c3f97c
Woan Chin <woanchin.tee@gmail.com>
2017-11-13 02:18:14 +0800

    didn't do much, i spent 30 minutes trying to get a 3 second videos. my life is a lie.

---
+++ transformation.m
@@ -0,0 +1,6 @@
+% Because shootingStar.m takes damn long for me, I'm just going to repeat
+% the extracting frames part here. For the actual implementation,
+% transformation is just a function that calculates the new position of x
+% and y.
+
+function 
