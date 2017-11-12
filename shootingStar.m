c85953690ce281229c27c7baeed9e1548acdc8f2
Woan Chin <woanchin.tee@gmail.com>
2017-11-12 23:54:08 +0800

    tried to solve the background issue to no avail. Gonna give up

--- shootingStar.m
+++ shootingStar.m
@@ -1,5 +1,9 @@
+%video = VideoReader('videos/3_40_to_3_42_jumping.mp4');
 video = VideoReader('videos/Bar swing FAIL.mp4');
-video.CurrentTime = 4.5; %lesser presence of ghost on the swing bars
+%video.CurrentTime = 219.5; %lesser presence of ghost on the swing bars
+video.CurrentTime = 4;
+%video = VideoReader('videos/traffic.mp4');
+
 currAxes = axes;
 frameMatrix = zeros(video.height, video.width, 3);
 firstFrame = zeros(video.height, video.width, 3);
@@ -8,36 +12,73 @@ movingObjects = zeros(video.height, video.width, 3);
 counter = 2;
 weightage = 1;
 
-if hasFrame(video)
-    frameMatrix = readFrame(video);
-    firstFrame = frameMatrix;
-    while hasFrame(video)
-        currentFrame = readFrame(video);
-        currentMatrix = (cast(frameMatrix, 'double') * weightage) ...
-                        + (cast(currentFrame, 'double'));
-        averageMatrix = currentMatrix / counter;
-        movingObjects = abs(cast(currentFrame, 'double') - averageMatrix);
-        
-        if(mod(counter, 5) == 0)
-            figure;
-            imshow(cast(movingObjects, 'uint8'));
-        end
-        frameMatrix = averageMatrix;
-        counter = counter + 1;
-        weightage = weightage + 1;
-    end
-else
-    disp('video is empty');
-end
+%if video.CurrentTime < 221.5
+    if hasFrame(video)
+        frameMatrix = readFrame(video);
+        firstFrame = frameMatrix;
+        while hasFrame(video)
+            currentFrame = readFrame(video);
+            currentMatrix = (cast(frameMatrix, 'double') * weightage) ...
+                            + (cast(currentFrame, 'double'));
+            averageMatrix = currentMatrix / counter;
+            movingObjects = abs(cast(currentFrame, 'double') - averageMatrix);
 
-firstMovingImage = abs(cast(firstFrame, 'double') - frameMatrix);
+            %if(mod(counter, 20) == 0)
+            %    figure;
+            %    imshow(cast(movingObjects, 'uint8'));
+            %end
+            frameMatrix = averageMatrix;
+            counter = counter + 1;
+            weightage = weightage + 1;
+        end
+    else
+        disp('video is empty');
+    end
+%end
+
+backgroundMatrix = frameMatrix;
+
+video.CurrentTime = 4;
+    if hasFrame(video)
+        frameMatrix = readFrame(video);
+        firstFrame = frameMatrix;
+        while hasFrame(video)
+            currentFrame = readFrame(video);
+            currentMatrix = (cast(frameMatrix, 'double') * weightage) ...
+                            + (cast(currentFrame, 'double'));
+            averageMatrix = currentMatrix / counter;
+            movingObjects = abs(cast(currentFrame, 'double') - backgroundMatrix);
+
+            %if(mod(counter, 20) == 0)
+            %    figure;
+            %    imshow(cast(movingObjects, 'uint8'));
+            %end
+            frameMatrix = averageMatrix;
+            counter = counter + 1;
+            weightage = weightage + 1;
+        end
+    else
+        disp('video is empty');
+    end
+    
+firstMovingImage = abs(cast(firstFrame, 'double') - backgroundMatrix);
 
 figH = figure;
+title('firstFrame');
 imshow(cast(firstFrame, 'uint8'));
+
 figI = figure;
+title('firstMovingImage');
 imshow(cast(firstMovingImage, 'uint8'));
 
 figJ = figure;
+title('frameMatrix');
 imshow(cast(frameMatrix, 'uint8'));
+
 figK = figure;
+title('MovingObjects');
 imshow(cast(movingObjects, 'uint8'));
+
+figL = figure;
+title('Background');
+imshow(cast(backgroundMatrix, 'uint8'));
