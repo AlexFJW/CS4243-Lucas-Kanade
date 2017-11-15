c4755c95ea789a1233d8b319b6ad0ec3e23bf159
Alex Fong <alexfongg@gmail.com>
2017-11-15 09:35:38 +0800

    Some internal cleaning up

---
+++ scene7/createScene7.m
@@ -0,0 +1,5 @@
+% creates scene 7 in sceneVideos directory
+function [matrix] = createScene7(humanVideoDirectory)
+    % extend human 
+
+    % write cells to video
---
+++ scene7/resizeOverTime.m
@@ -0,0 +1,6 @@
+% Resize video matrix cells over time
+
+% params video: full video
+% params endSize: fraction of the original size, eg. 0.3
+% returns resizedResult: the resized video matrix cells
+function [resizedResult] = resizeOverTime(videoCells, endSize)
