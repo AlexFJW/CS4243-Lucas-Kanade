6fe1f067806c63c7db39f59ab3db5f1dc035eb82
Alex Fong <alexfongg@gmail.com>
2017-11-15 23:19:28 +0800

    added skeleton for mergeCellsWithTranslation test + renamed old test

---
+++ tests/testMergeCellsWithTranslation.m
@@ -0,0 +1,11 @@
+
+% insert human cells
+
+% insert bg cells
+
+% rescale to human to 30% of bg
+
+% move from left to right
+% start = (0, midY) x,y
+% end = (endX, midY) x,y
+% mergeCellsWithTranslation(humanCells, bgCells, ) 
