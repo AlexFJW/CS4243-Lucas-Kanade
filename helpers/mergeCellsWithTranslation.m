451d397b437953bec3afa7244c5ec3687747e05c
Alex Fong <alexfongg@gmail.com>
2017-11-16 07:50:24 +0800

    More bonfires
    Fixed some bugs in code, testMergeCellsWithTranslation compiles now
    but.... breaks while running.

--- helpers/mergeCellsWithTranslation.m
+++ helpers/mergeCellsWithTranslation.m
@@ -5,7 +5,8 @@
 
 % params overlay: overlay cells (must have same size as background)
 % params background: background cells (same as above)
-% params startX: x coordinate of overlay's start position (overlay's center point will be here in the bg)
+% params startX: x coordinate of overlay's start position
+% (overlay's center point will be here in the bg, MUST be in bounds of frame)
 % params startY: y coordinate of overlay's start position (see above)
 % params destX: x coordinate of overlay's end position (see above)
 % params destY: y coordinate of overlay's end position (see above)
@@ -16,8 +17,11 @@ function [merged] = mergeCellsWithTranslation(overlay, background, startX, start
     if (numBgFrames ~= numOverlayFrames)
         % overlay must be of equal length to base
         % we don't want to see disappearing overlays or backgrounds halfway through
-        error('overlay does not have same number of frames as background.
-         may lead to overlay disappearing or background disappearing...');
+        disp('overlay size')
+        numOverlayFrames
+        disp('bg size')
+        numBgFrames
+        error('overlay does not have same number of frames as background. overlay or bg might disappear.');
     end
 
     merged = cell(1, numBgFrames);
@@ -30,8 +34,8 @@ function [merged] = mergeCellsWithTranslation(overlay, background, startX, start
     centerY = startY;
     for i = 1:numOverlayFrames
         bgFrame = background{i};
-        overlayFrame = overlay{i}:
-
+        overlayFrame = overlay{i};
+        [bgHeight, bgWidth, ~] = size(bgFrame);
         [overlayHeight, overlayWidth, ~] = size(overlayFrame);
         % position of overlay's left edge in the bg
         xLeft = centerX - overlayWidth/2;
@@ -45,26 +49,46 @@ function [merged] = mergeCellsWithTranslation(overlay, background, startX, start
         % nudge overlay's position a bit to land whole pixels
         % if dX positive, ceil. else floor
         if (dX >= 0)
-            xLeft = ceil(xLeft)
-            xRight = ceil(xRight)
+            xLeft = ceil(xLeft);
+            xRight = ceil(xRight);
         else
-            xLeft = floor(xLeft)
-            xRight = floor(xRight)
+            xLeft = floor(xLeft);
+            xRight = floor(xRight);
         end
 
         if (dY >= 0)
-            yTop = ceil(yTop)
-            yBottom = ceil(yBottom)
+            yTop = ceil(yTop);
+            yBottom = ceil(yBottom);
         else
-            yTop = floor(yTop)
-            yBottom = floor(yBottom)
+            yTop = floor(yTop);
+            yBottom = floor(yBottom);
         end
         % shift for next iteration
         centerX = centerX + dX;
         centerY = centerY + dY;
 
         % window from bg that will be replaced by overlay
-        bgWindow = bgFrame(yTop:yBottom, xLeft:xRight, : );
+        % top, bottom, left, right may be outside of frame, drop those parts
+        max(0, yTop)
+        min(bgHeight, yBottom)
+        max(0, xLeft)
+        min(bgWidth, xRight)
+
+        bgWindow = bgFrame(max(0, yTop):min(bgHeight, yBottom), ...
+                           max(0, xLeft):min(bgWidth, xRight), ...
+                           1:3);
+
+        [windowHeight, windowWidth, ~] = size(bgWindow);
+
+        % trim parts of overlay frame that are out of window
+        % warning... floor may be the wrong choice
+        halfOverlayWidth = floor(overlayWidth/2);
+        halfOverlayHeight = floor(overlayHeight/2);
+        leftBound = floor(max(0, halfOverlayWidth - centerX));
+        rightBound = min(overlayWidth, leftBound + windowWidth);
+        topBound = floor(max(0, halfOverlayHeight - centerY));
+        bottomBound = min(overlayHeight, topBound + windowHeight);
+        overlayFrame = overlayFrame(topBound:bottomBound, leftBound:rightBound, 1:3);
 
         % Get location of the black pixels in all channels (overlay's coordinates)
         blackR = overlayFrame(:,:,1) == 0;
@@ -74,10 +98,10 @@ function [merged] = mergeCellsWithTranslation(overlay, background, startX, start
         blackPixels_Overlay = find(blackR & blackG & blackB);
 
         % fill overlay's black pixels with pixels from bg
-        overlayFrame(blackPixels) = bgWindow(blackPixels);
+        overlayFrame(blackPixels_Overlay) = bgWindow(blackPixels_Overlay);
 
         % paste overlay onto background
-        bgFrame(yTop:yBottom, xLeft:xRight, : ) = overlayFrame;
+        bgFrame(yTop:yBottom, xLeft:xRight, 1:3) = overlayFrame;
 
         merged{i} = bgFrame;
     end
--- tests/testMergeCellsWithTranslation.m
+++ tests/testMergeCellsWithTranslation.m
@@ -1,11 +1,22 @@
-
 % insert human cells
+video = VideoReader('videos/filtered/human1_3_out_100.mp4');
+humanCells = videoToCells(video);
 
 % insert bg cells
-
+bg = VideoReader('videos/trimmed/shooting_stars_galaxy.mp4');
+bgCells = videoToCells(bg);
 % rescale to human to 30% of bg
+[bgHeight, bgWidth, ~] = size(bgCells{1});
+humanCells = resizeChild(humanCells, bgHeight, bgWidth, 0.3);
+
+[~, numBgFrames] = size(bgCells);
+% extend to same video length
+humanCells = extendVideo(humanCells, numBgFrames);
 
 % move from left to right
 % start = (0, midY) x,y
 % end = (endX, midY) x,y
-% mergeCellsWithTranslation(humanCells, bgCells, ) 
+midY = floor(bgHeight/2);
+merged =  mergeCellsWithTranslation(humanCells, bgCells, 0, midY, bgWidth, midY);
+
+videoCellsToMp4(merged, video.Framerate, 'test_output/mergedWithTranslateTest.mp4');
