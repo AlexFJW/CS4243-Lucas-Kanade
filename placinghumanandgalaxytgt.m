89df6a4f89937b8f7195497a27f4f4cd26c605ff
Jorel Tay <jorel_31@hotmail.com>
2017-11-16 21:05:56 +0800

    final galaxy

--- placinghumanandgalaxytgt.m
+++ placinghumanandgalaxytgt.m
@@ -27,11 +27,12 @@ bgPart1 = bgCells(1:89);
 humanPart1(5:15) = overlayColor(humanPart1(5:15),[255 0 0],0.5);
 humanPart1(30:45) = overlayColor(humanPart1(30:45),[0 0 255],0.5);
 humanPart1(60:75) = overlayColor(humanPart1(60:75),[0 255 0],0.5);
+humanPart1 = rotateOverTime(humanPart1, 50);
 humanPart1 = resizeImmediately(humanPart1, 2.5);
 humanPart1= resizeOverTime(humanPart1, 0.5);
 
 lastX = 1280; lastY = -50;
-nextX = 700; nextY = 400;
+nextX = 700; nextY = 300;
 [merged1, lastX, lastY] = mergeCellsWithTranslation(humanPart1, bgPart1, lastX, lastY, nextX, nextY);
 
 videoCellsToMp4(merged1, bgVid.Framerate, 'jorelvideo/humangalaxy.mp4'); % test code
