a8f8efb28f6767abb5e0e42037884a587eddd57b
Jorel Tay <jorel_31@hotmail.com>
2017-11-17 15:49:06 +0800

    trying to find bug. not sure where is it.

--- mergingScenes/testmergeScene.m
+++ mergingScenes/testmergeScene.m
@@ -2,6 +2,6 @@ tmp = dir('videos/results/human1/*.mp4');        % retrieve .mp4 files
 videolist = {tmp.name}';   % sort this list if necessary! sort(videoList) might work
 mergeScene(videolist,'videos/results/shooting_star_1.mp4');
 
-tmp = dir('videos/results/human2/*.mp4');        % retrieve .mp4 files
-videolist = {tmp.name}';   % sort this list if necessary! sort(videoList) might work
+temp = dir('videos/results/human2/*.mp4');        % retrieve .mp4 files
+videolist = {temp.name}';   % sort this list if necessary! sort(videoList) might work
 mergeScene(videolist,'videos/results/shooting_star_2.mp4');
