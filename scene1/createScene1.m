9edb9a0b84aa2b3df8178b49748d7153cbc698a1
Jorel Tay <jorel_31@hotmail.com>
2017-11-17 22:23:49 +0800

    done done

--- scene1/createScene1.m
+++ scene1/createScene1.m
@@ -15,25 +15,25 @@ function [] = createScene1(humanVideoPath, outputVideoPath)
     humanCells = resizeChild(humanCells, bgHeight, bgWidth, 0.45);
 
     % rotate human cells
-    humanCells = rotateCells(humanCells, -90, true);
+    %humanCells = rotateCells(humanCells, -90, true);
 
     %extendVideo
     humanCells = extendVideo(humanCells, totalBgFrames);
 
     %frame1
-    humanCells = horizontalFlipCells(humanCells);
+    humanCells = rotateCells(humanCells, 20, false);
     humanPart1 = humanCells(1:89);
     bgPart1 = bgCells(1:89);
 
-    humanPart1(5:15) = overlayColor(humanPart1(5:15),[255 0 0],0.5);
-    humanPart1(30:45) = overlayColor(humanPart1(30:45),[0 0 255],0.5);
-    humanPart1(60:75) = overlayColor(humanPart1(60:75),[0 255 0],0.5);
+    humanPart1(10:20) = overlayColor(humanPart1(10:20),[255 0 0],0.5);
+    humanPart1(35:50) = overlayColor(humanPart1(35:50),[0 0 255],0.5);
+    humanPart1(65:80) = overlayColor(humanPart1(65:80),[0 255 0],0.5);
     humanPart1 = rotateOverTime(humanPart1, 50);
     humanPart1 = resizeImmediately(humanPart1, 2.5);
     humanPart1= resizeOverTime(humanPart1, 0.5);
 
-    lastX = 1280; lastY = -50;
-    nextX = 700; nextY = 300;
+    lastX = 1280; lastY = 0;
+    nextX = 700; nextY = 350;
     [merged, lastX, lastY] = mergeCellsWithTranslation(humanPart1, bgPart1, lastX, lastY, nextX, nextY,false, NaN);
 
     merged = squeezeBrightnessContrastForCells(merged);
