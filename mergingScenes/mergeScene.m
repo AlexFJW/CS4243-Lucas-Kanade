6c96cabd46b90b234e6264c69c9ecbb03bf00c35
Jorel Tay <jorel_31@hotmail.com>
2017-11-17 15:49:46 +0800

    no message

--- mergingScenes/mergeScene.m
+++ mergingScenes/mergeScene.m
@@ -1,8 +1,8 @@
-function [] = mergeScene(videolist,destination)
+function [] = mergeScene(vlist,destination)
     outputVideo = VideoWriter(destination, 'MPEG-4');
     open(outputVideo);
-    for i = 1:size(videolist)
-        inputVideo = VideoReader(videolist{i});
+    for i = 1:size(vlist)
+        inputVideo = VideoReader(vlist{i});
         while hasFrame(inputVideo)
             writeVideo(outputVideo, readFrame(inputVideo));
         end
