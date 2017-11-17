380ce9ec194515a123c4570cbefdeda06a1b1bbb
Nicholette <nekonekonik@gmail.com>
2017-11-17 10:23:45 +0800

    Tweaked functions for scene 3 and 4

--- scene4/testScene4.m
+++ scene4/testScene4.m
@@ -1,1 +1,5 @@
-createScene4('videos/filtered/human1_1_out_60_clean.mp4',0.45, false, -40, 5, 8, 'videos/scene4.2', true)
+% % For snow guy
+% createScene4('videos/filtered/human1_3_out_100.mp4',0.9, false, -40, 160, 120, 1, 1, 5, 8, 'videos/scene4.2', false)
+
+% For swing guy
+createScene4('videos/filtered/swing_out_trimmed_t55_s28_e36_clean.mp4',0.9, true, -60, -30, -20, 5, 5, 2, 3, 'videos/scene4.2.human2.2', false)
